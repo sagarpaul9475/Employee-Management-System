ce2c1eb152744f14c5b43d51b3218378d6daa7e7
sagarpaul9475 <sagarpaul9475@gmail.com>
2026-08-02 23:53:40 +0530

    Remove CrossOrigin annotation from EmployeeController

--- springboot-backend/src/main/java/com/EmployeeManagementSystem/springboot_backend/controller/EmployeeController.java
+++ springboot-backend/src/main/java/com/EmployeeManagementSystem/springboot_backend/controller/EmployeeController.java
@@ -6,7 +6,6 @@ import java.util.Map;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -21,7 +20,6 @@ import com.EmployeeManagementSystem.springboot_backend.repository.EmployeeReposi
 
 @RestController
 @RequestMapping("/api/v1/")
-@CrossOrigin(origins = "*")
 public class EmployeeController {
 
 	@Autowired
