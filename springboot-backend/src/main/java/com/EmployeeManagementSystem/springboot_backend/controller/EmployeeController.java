c77756e8d5a61112f26ec94601acc7e6522392d6
sagarpaul9475 <sagarpaul9475@gmail.com>
2026-08-02 23:42:57 +0530

    fix cors

--- springboot-backend/src/main/java/com/EmployeeManagementSystem/springboot_backend/controller/EmployeeController.java
+++ springboot-backend/src/main/java/com/EmployeeManagementSystem/springboot_backend/controller/EmployeeController.java
@@ -6,6 +6,7 @@ import java.util.Map;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -20,6 +21,7 @@ import com.EmployeeManagementSystem.springboot_backend.repository.EmployeeReposi
 
 @RestController
 @RequestMapping("/api/v1/")
+@CrossOrigin(origins = "*")
 public class EmployeeController {
 
 	@Autowired
